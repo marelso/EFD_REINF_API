8584f6283c161a846c687f94d516cd5d6596bc9c
marelso <klein.marcelojr@gmail.com>
2022-05-08 22:08:49 -0300

    Desenvolvimento de método get

--- TesteRFB_API/principal.cs
+++ TesteRFB_API/principal.cs
@@ -97,7 +97,66 @@ namespace TesteRFB_API
         {
             string response = string.Empty;
 
-            return response;
+            try
+            {
+                #region Send request
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(adress);
+                request.Method = "GET";
+                request.Timeout = 2400000;
+                request.Accept = "application/xml";
+                request.ContentType = "application/xml";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(content);
+                }
+                #endregion
+
+                #region Get response
+                HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                #endregion
+
+                #region Xml desserialization and response filter
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(response);
+                XmlNode root = doc.DocumentElement;
+
+                int code = Convert.ToInt32(string.Format("{0}", root.SelectSingleNode("cdResposta").InnerXml.Trim()));
+                switch (code)
+                {
+                    case 1:
+                        MessageBox.Show(string.Format("Indica que o Lote ainda está em processamento pela EFD-REINF."));
+                        break;
+                    case 2:
+                        MessageBox.Show(string.Format("O Lote foi processado. Todos eventos foram processados com sucesso."));
+                        break;
+                    case 3:
+                        MessageBox.Show(string.Format("O Lote foi processado. Porém possui um ou mais eventos com ocorrências."));
+                        break;
+                    case 8:
+                        MessageBox.Show(string.Format("A consulta possui ocorrências e a busca do Lote não foi executada. Verificar ocorrências no xml de retorno."));
+                        break;
+                    case 9:
+                        MessageBox.Show(string.Format("A consulta foi executada, porém o Lote não foi encontrado."));
+                        break;
+                    case 99:
+                    default:
+                        MessageBox.Show(string.Format("Erro interno na EFD-REINF."));
+                        break;
+                }
+                #endregion
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Erro:\n {0}", ex.Message));
+                return string.Empty;
+            }
         }
     }
 }
